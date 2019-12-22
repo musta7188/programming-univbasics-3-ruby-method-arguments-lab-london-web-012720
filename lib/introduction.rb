499427a965b6d607c858181e66eec9ac8c2c46c7
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-22 00:25:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ end
 introduction_with_language("Dan", "Ember.js")
 
 def introduction_with_language_optional(name = "Edwin")
-  puts "Hi, my name is #{name} and I am learning to program in Ruby"
+  puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
 
 introduction_with_language_optional()
